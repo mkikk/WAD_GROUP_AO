7c109518a2240ddc97cac14849ed20ad64159f11
kertucarina <kertu-carina.kallaste@ut.ee>
2021-10-21 16:17:16 +0300

    jquery code and json integration

---
+++ res/js/script.js
@@ -0,0 +1,44 @@
+$(function() {
+
+    // Retrieving the posts from the given url and outputting the posts to the html page.
+    $.get("https://api.npoint.io/9a36eccdabc48a0eef0a", function(posts) {
+        
+        let div_box = $('<div class="box">');
+        
+        for (post of posts) {
+
+            let div_post = $('<div class="post">');
+            let div_top = $('<div class="top">');
+            let div_content = $('<div class="content">');
+            let div_likebutton = $('<div class="likebutton">');
+
+            let author = $('<p>').text(post.author);
+            let time = $('<p class="textgrey">').text(post.time);
+            let title = $('<h3>').text(post.title);
+            let body = $('<p>').text(post.body);
+
+            let userPhoto = $('<img class="userpic">').src(post.author_path).alt("User");
+            let photo = $('<img class="contentpic">').src(post.photo_path).alt(post.photo_path);
+            let likeButton = $('<img class="likebuttonpic">').src(post.like_path).alt("Like button");
+
+            div_top.append(userPhoto)
+            div_top.append(author)
+            div_top.append(time)
+
+            div_content.append(photo)
+            div_content.append(title)
+            div_content.append(body)
+
+            div_likebutton.append(likeButton)
+
+            div_post.append(div_top)
+            div_post.append(div_content)
+            div_post.append(div_likebutton)
+
+            div_box.append(div_post)
+        }
+
+        $('.section').append(div_box)
+    })
+
+});
